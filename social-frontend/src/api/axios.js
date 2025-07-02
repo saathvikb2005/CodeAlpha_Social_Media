01b3457540ef6bc6b3d1b9efcc0cea0a4de2c109
BACHALI SAATHVIK <saathvikbachali@gmail.com>
2025-07-02 22:46:30 +0530

    frontend

--- social-frontend/src/api/axios.js
+++ social-frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+// Use live URL from .env or fallback to localhost
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000/api/';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
